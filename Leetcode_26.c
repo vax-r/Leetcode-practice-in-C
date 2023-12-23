db3b6de80938e5fabc34ff1ee86eca71e9ad5f56
vax-r <richard120310@gmail.com>
2023-12-23 22:46:36 +0800

    Leetcode 26 Remove Duplicates from Sorted Array

---
+++ Leetcode_26.c
@@ -0,0 +1,8 @@
+int removeDuplicates(int* nums, int numsSize) {
+    int k = 0;
+    for (int i = 1; i < numsSize; i++) {
+        if (nums[k] != nums[i])
+            nums[++k] = nums[i];
+    }
+    return k + 1;
+}
