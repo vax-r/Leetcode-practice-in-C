c2c7b34bc152b805bfe048296f657077ddf4b03a
vax-r <richard120310@gmail.com>
2024-02-25 11:23:20 +0800

    Applying pointer to pointer method when merging lists
    According to Linus Torvalds's suggestion on how to apply
    pointer to pointer, we change the implementation of leetcode
    21. Merge Two Lists.
    
    Instead of using an if statement to check whether we're
    dealing with the head pointer, use simply use a pointer to
    pointer which pointing to head first, then manipulate every
    pointer to ListNode when we're traversing the linked lists.

--- Leetcode_21.c
+++ Leetcode_21.c
@@ -6,38 +6,25 @@
  * };
  */
 struct ListNode* mergeTwoLists(struct ListNode* list1, struct ListNode* list2){
-    if ( !list1 )
+
+    if (!list1)
         return list2;
-    if ( !list2 )
+    if (!list2)
         return list1;
-    struct ListNode *cur_1, *cur_2, *merge_head, *merge_tail;
-    cur_1 = list1;
-    cur_2 = list2;
-    if ( cur_1->val <= cur_2->val ) {
-        merge_head = cur_1;
-        merge_tail = cur_1;
-        cur_1 = cur_1->next;
-    }
-    else {
-        merge_head = cur_2;
-        merge_tail = cur_2;
-        cur_2 = cur_2->next;
-    }
-    while ( cur_1 && cur_2 ) {
-        if ( cur_1->val <= cur_2->val ) {
-            merge_tail->next = cur_1;
-            merge_tail = cur_1;
-            cur_1 = cur_1->next;
-        }
-        else {
-            merge_tail->next = cur_2;
-            merge_tail = cur_2;
-            cur_2 = cur_2->next;
+
+    struct ListNode *head = NULL;
+    struct ListNode **tail = &head;
+    while (list1 && list2) {
+        if (list1->val <= list2->val) {
+            *tail = list1;
+            tail = &(list1->next);
+            list1 = list1->next;
+        } else {
+            *tail = list2;
+            tail = &(list2->next);
+            list2 = list2->next;
         }
     }
-    if ( cur_1 )
-        merge_tail->next = cur_1;
-    if ( cur_2 )
-        merge_tail->next = cur_2;
-    return merge_head;
+    *tail = (struct ListNode *) ((uintptr_t) list1 | (uintptr_t) list2);
+    return head;
 }
